eb41d7f12c31835054c4b9acef7abf8c920ba36d
Cauã de Andrade <104733922+caua075@users.noreply.github.com>
2024-08-14 00:16:15 -0300

    Update conexao.php

--- crud/conexao.php
+++ crud/conexao.php
@@ -1,9 +1,11 @@
 <?php
-$password = "senha_da_nasa";
-$dbname = "trab_ids";
+$host = 'localhost';
+$user = 'root';
+$password = 'senha_da_nasa';
+$dbname = 'trab_ids';
 
 // Criar conexão
-$conn = new mysqli($password, $dbname);
+$conn = new mysqli('nome_do_host', 'usuario', 'senha', 'nome_do_banco');
 
 // Checar conexão
 if ($conn->connect_error) {
